a3e7914d642271d9990fe6baea6844729cef895c
Sebastian <sebastianpedroza442@gmail.com>
2025-05-03 14:41:13 -0500

    Commit inicial del proyecto

---
+++ src/main/java/com/example/EcoSfera/EcoSferaApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcoSfera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoSferaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoSferaApplication.class, args);
+	}
+
+}

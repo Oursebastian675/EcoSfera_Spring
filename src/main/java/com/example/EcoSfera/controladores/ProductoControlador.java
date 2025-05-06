5a243877824f877faa687e96fb394e7923b52045
Sebastian <sebastianpedroza442@gmail.com>
2025-05-06 18:48:39 -0500

    Funcionalidad al proyecto

--- src/main/java/com/example/EcoSfera/controladores/ProductoControlador.java
+++ src/main/java/com/example/EcoSfera/controladores/ProductoControlador.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/productos")
+@RequestMapping("/productos")
 
 public class ProductoControlador {
     @Autowired
